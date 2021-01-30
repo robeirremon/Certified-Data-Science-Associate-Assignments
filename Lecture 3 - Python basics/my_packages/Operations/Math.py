ff6baceea3bcef7301586061e1a2bbce3a8a8db9
Robeir Remon <robeir.remon@gmail.com>
2021-01-30 17:11:07 +0200

    Add Assignments for all previous lectures from 2 to 5

---
+++ Lecture 3 - Python basics/my_packages/Operations/Math.py
@@ -0,0 +1,5 @@
+def get_average(lst):
+    total=0
+    for i in lst:
+        total+=i
+    return total/len(lst)
---
+++ Lecture 3 - Python basics/my_packages/Transformers/StringTransformer.py
@@ -0,0 +1,5 @@
+def reverse_string(str):
+    return str[::-1]
+
+def capitalize_string(str):
+    return str.capitalize()
